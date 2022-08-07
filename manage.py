eda7dac9a4c00f501219842e54708a2c188cadb3
Minty Hippo <clayton.wieben@gmail.com>
2022-08-06 18:04:44 -0600

    fiddled with the migrate dependency

--- manage.py
+++ manage.py
@@ -1,16 +1,15 @@
 def deploy():
     """Run deployment tasks."""
     from app import create_app, db
-    from flask_migrate import upgrade, migrate, init, stamp
+    from flask_migrate import Migrate, upgrade, init, stamp
     from models import Paragraphs
 
     app = create_app()
     app.app_context().push()
-
     db.create_all()
 
     stamp()
-    migrate()
+    migrate = Migrate(app, db)
     upgrade()
 
 deploy()
